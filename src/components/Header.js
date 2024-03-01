2f2e5da8cb9b4ab267bbd769d583331e497c61c6
titus-mumo <tituskennedy74@gmail.com>
2024-03-01 11:21:20 +0300

    file configuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
