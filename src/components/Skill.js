cab354010aea947d66cb7a3fb6a66a2c6eaf6eb9
titus-mumo <tituskennedy74@gmail.com>
2024-03-03 11:03:46 +0300

    skill bar

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,11 +3,13 @@ import React from "react";
 export const Skill = ({ skill, color }) => {
     const textColor = 'black';
     return (
-        <div>
+        <div className='flex flex-rol items-center'>
             <p 
-            className= "text-white text-sm m-1 py-1 px-1.5 rounded-xl bg-200"
-            style={{background: color}}
+            className= "text-black text-md m-1 py-1 px-1.5 rounded-xl bg-200"
             >{skill}</p>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <div class="bg-blue-600 h-2.5 rounded-full" style={{width: color}}></div>
+            </div>
         </div>
     );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,27 +3,27 @@ import React from "react";
 
 export const Skills = () => {
     const collections = [
-        { "skill": "JavaScript", "color": "#77C3EC" },
-        { "skill": "React", "color": "red" },
-        { "skill": "Python", "color": "green" },
-        { "skill": "HTML", "color": "black" },
-        { "skill": "CSS", "color": "brown" },
-        { "skill": "Node.js", "color": "green" },
-        { "skill": "Django", "color": "violet" },
-        { "skill": "PostgreSQL", "color": "orange" },
-        { "skill": "Machine Learning", "color": "purple" }
+        { "skill": "JavaScript", "color": '80%' },
+        { "skill": "React", "color": '85%'},
+        { "skill": "Python", "color": '90%' },
+        { "skill": "HTML", "color": '95%' },
+        { "skill": "CSS", "color": '95%'},
+        { "skill": "Node.js", "color": '85%' },
+        { "skill": "Django", "color": '90%' },
+        { "skill": "PostgreSQL", "color": '95%'},
+        { "skill": "Machine Learning", "color": '75%' }
     ];
 
     const learning = [
-        {'skill':"TypeScript", "color": "brown"},
-        {"skill": "PHP", "color":"green"},
-        {"skill":"AWS", "color":"orange"}
+        {'skill':"TypeScript", "color": '50%'},
+        {"skill": "PHP", "color":'50%'},
+        {"skill":"AWS", "color":'45%'}
     ]
 
     return (
         <div className="flex flex-col justify-center w-full">
             <p className='text-3xl font-semibold text-yellow-500'>SKILLS</p>
-            <div className="flex flex-wrap justify-center w-full">
+            <div className="flex flex-col justify-center sm:w-90vw w-90 m-auto">
                 {collections.map((collection, index) => {
                     return (
                         <Skill key={index} skill={collection.skill} color={collection.color} />
@@ -31,7 +31,7 @@ export const Skills = () => {
                 })}
             </div>
             <p className='text-3xl font-semibold text-yellow-500'>LEARNING</p>
-            <div className="flex flex-wrap justify-around">
+            <div className="flex flex-col justify-center w-auto m-auto">
                 {learning.map((collection, index) => {
                     return (
                         <Skill key={index} skill={collection.skill} color={collection.color} />
